fe038d386615cb81040cada1989a8ffdefe3fb64
Xuan Pham <hpham024@uottawa.ca>
2024-11-04 21:34:28 -0500

    modified test server to listen on public address

--- BAND1/src/backend/TestServer.py
+++ BAND1/src/backend/TestServer.py
@@ -1,7 +1,7 @@
 import socket;
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('localhost', 5555))
+server.bind((socket.gethostname(), 5555))
 server.listen(1)
 print("Server started and listening on", server.getsockname())
 
